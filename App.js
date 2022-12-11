db6266cd06108e5361633da0837ff846f726c52d
Pho <phonum5@gmail.com>
2022-12-11 20:27:03 +0000

    Home and Notifications screens

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+// component imports
+import Header from "./components/Header";
+import Home from './screens/Home'
+import Notifications from './screens/Notifications'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <Notifications/>
+            <StatusBar style="auto"/>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        marginTop: Platform.OS === 'ios' ? 0 : 20,
+        flex: 1,
+    },
 });
